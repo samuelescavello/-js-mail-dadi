871dd02ddfb5c29b7caa2d6e917bea833ed49f94
samuelescavello <scavellosamuele23@gmail.com>
2024-03-11 17:05:38 +0100

    dadi

--- js/script.js
+++ js/script.js
@@ -5,21 +5,21 @@
 let email = ['samu@gmail.com', 'luca@gmail.com', 'lorenzo@gmail.com', 'nicola@gmail.com', 'woitec@gmail.com'];
 
 const button = document.querySelector('.btn-danger')
-button.addEventListener('click', function(){
-    
-    let userEmail = document.getElementById('mail').value ;
-    let trovato = false ;
-    for(let i = 0 ; i < email.length ; i++){
-        if(userEmail === email[i]){
-            trovato = true ;
+button.addEventListener('click', function () {
+
+    let userEmail = document.getElementById('mail').value;
+    let trovato = false;
+    for (let i = 0; i < email.length; i++) {
+        if (userEmail === email[i]) {
+            trovato = true;
         }
     }
 
     const output = document.getElementById('output')
 
-    if(trovato){
+    if (trovato) {
         output.innerHTML = 'sei stato invitato!!!'
-    }else{
+    } else {
         output.innerHTML = ' mi dispiace , non sei stato invitato!!!'
     }
 
@@ -30,3 +30,36 @@ button.addEventListener('click', function(){
 //  per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
+// function getRndInteger(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1) ) + min;
+// }
+
+let gameDadi = document.getElementById('game2')
+game2.addEventListener('click', function () {
+    let userRandom = getRndInteger(1, 6);
+    let pcRandom = getRndInteger(1, 6)
+    console.log(userRandom)
+    console.log(pcRandom)
+
+    if (userRandom === pcRandom) {
+        num.innerHTML = `
+        <p> hai PAREGGIATO il tuo numero é "${userRandom}"... il computer aveva "${pcRandom}"</p>
+        `
+    }else if (userRandom > pcRandom) {
+        num.innerHTML = `
+    <p> hai VINTO il tuo numero é "${userRandom}"... il computer aveva "${pcRandom}"</p>
+    `
+    } else (userRandom < pcRandom)
+    num.innerHTML = `
+    <p> hai PERSO il tuo numero é "${userRandom}"... il computer aveva "${pcRandom}"</p>
+    `
+
+
+
+
+})
+
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
